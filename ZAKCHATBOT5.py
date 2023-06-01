fcc385a67d3718956411aa70e38021e9663966ba
beamerrr2 <121476738+beamerrr2@users.noreply.github.com>
2023-06-01 00:59:46 -0400

    Update ZAKCHATBOT5.py

--- ZAKCHATBOT5.py
+++ ZAKCHATBOT5.py
@@ -9,7 +9,6 @@ import datetime
 import pinecone
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-from openai.embeddings_utils import cosine_similarit
 import logging
 from dataclasses import dataclass, field
 import openai
